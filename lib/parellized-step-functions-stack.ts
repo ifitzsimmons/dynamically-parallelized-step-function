05b82a59e8130e1fa336909433687eaa210c0fd4
Ian Fitzsimmons <imf35@drexel.edu>
2022-06-25 17:22:20 -0400

    Checkpoint

--- lib/parellized-step-functions-stack.ts
+++ lib/parellized-step-functions-stack.ts
@@ -1,16 +1,65 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as sfnTasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { join } from 'path';
 
 export class ParellizedStepFunctionsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const createTimers = new lambda.Function(this, 'CreateOrderTimeouts', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'calculate-timers.handler',
+      code: lambda.Code.fromAsset(join(__dirname, 'lambdas')),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ParellizedStepFunctionsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const checkTimeout = new lambda.Function(this, 'CheckOrderStatus', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'check-status.handler',
+      code: lambda.Code.fromAsset(join(__dirname, 'lambdas')),
+    });
+
+    const successState = new sfn.Succeed(this, 'Timers Checked');
+
+    const calculateTimerTask = new sfnTasks.LambdaInvoke(this, 'CreateTimers', {
+      lambdaFunction: createTimers,
+      payload: sfn.TaskInput.fromObject({
+        orderId: sfn.JsonPath.stringAt('$.orderId'),
+        createdTimestamp: sfn.JsonPath.numberAt('$.createdTimestamp'),
+      }),
+      resultPath: sfn.JsonPath.stringAt('$.timers')
+    });
+
+    const checktimeoutTask = new sfnTasks.LambdaInvoke(this, 'CheckTimeout', {
+      lambdaFunction: checkTimeout,
+      payload: sfn.TaskInput.fromObject({
+        orderId: sfn.JsonPath.stringAt('$.orderId'),
+        expectedState: sfn.JsonPath.stringAt('$.expectedState'),
+        alarmName: sfn.JsonPath.stringAt('$.alarmName'),
+      }),
+      resultPath: "$"
+    });
+
+    const waitState = new sfn.Wait(this, 'Wait for Timer', {
+      time: sfn.WaitTime.timestampPath('$.waitTimestamp'),
+    });
+
+    const map = new sfn.Map(this, 'Parallelized timers', {
+      inputPath: sfn.JsonPath.entirePayload,
+      itemsPath: sfn.JsonPath.stringAt('$.timers.Payload'),
+      maxConcurrency: 50,
+    });
+
+    map
+      .iterator(waitState)
+      .iterator(checktimeoutTask)
+
+    const definition = calculateTimerTask.next(map).next(successState);
+
+    new sfn.StateMachine(this, 'OmsTimeoutAlerts', {
+      definition,
+    });
   }
 }
--- test/parellized-step-functions.test.ts
+++ test/parellized-step-functions.test.ts
@@ -7,7 +7,7 @@
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new ParellizedStepFunctions.ParellizedStepFunctionsStack(app, 'MyTestStack');
+//   const stack = new ParellizedStepFunctions.StaticParellizedStepFunctionsStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
