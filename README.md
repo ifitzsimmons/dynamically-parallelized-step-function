7547ca7f22f00ec5c54900511e14d28773d5badb
Ian Fitzsimmons <imf35@drexel.edu>
2022-08-27 23:59:14 -0400

    Dynamically Parallelized Step Function

--- README.md
+++ README.md
@@ -12,3 +12,41 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+## Run the state machine
+
+This stack builds a state machine with dynamically parallelized workflows named `OrdersSlaAlerts`. You can test this state machine with the following input:
+
+```json
+{
+  "orderId": "myOrder",
+  "timers": [
+    {
+      "expectedState": "PaymentReceived",
+      "timeoutInSeconds": 30,
+      "alarmName": "PaymentNotReceived"
+    },
+    {
+      "expectedState": "OrderAccepted",
+      "timeoutInSeconds": 36,
+      "alarmName": "OrderNotAccepted"
+    },
+    {
+      "expectedState": "OrderInProgress",
+      "timeoutInSeconds": 42,
+      "alarmName": "OrederNotStarted"
+    },
+    {
+      "expectedState": "OrderReadyForPickup",
+      "timeoutInSeconds": 48,
+      "alarmName": "MissedPickupTime"
+    },
+    {
+      "expectedState": "OrderOutForDelivery",
+      "timeoutInSeconds": 54,
+      "alarmName": "MissedDelivery"
+    }
+  ]
+}
+```
+The `timers` list can be as long or short as you want, and you can adjust the timeout to virtually any time you want. The max wait time for a wait state in a step function is one year, so go nuts if you really want to!
--- lib/parellized-step-functions-stack.ts
+++ lib/parellized-step-functions-stack.ts
@@ -4,62 +4,107 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
 import * as sfnTasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { join } from 'path';
+import { LambdaInvoke } from 'aws-cdk-lib/aws-stepfunctions-tasks';
 
 export class ParellizedStepFunctionsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const createTimers = new lambda.Function(this, 'CreateOrderTimeouts', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      handler: 'calculate-timers.handler',
-      code: lambda.Code.fromAsset(join(__dirname, 'lambdas')),
+    const convertTimeoutToTimestampTask = this.getConvertTimeoutTask();
+    const waitState = new sfn.Wait(this, 'Wait for Timer', {
+      time: sfn.WaitTime.timestampPath('$.waitTimestamp.isoString'),
+    });
+    const checktimeoutTask = this.getCheckTimeoutTask();
+
+    // States that handle pass/fail (these are mocks for simplicity)
+    const timeoutPass = new sfn.Pass(this, 'Timeout Pass');
+    const timeoutFail = new sfn.Pass(this, 'Timeout Fail');
+    const choiceState = new sfn.Choice(this, 'SLA Met?')
+      .when(sfn.Condition.stringEquals('$.Payload.status', 'PASSED'), timeoutPass)
+      .when(sfn.Condition.stringEquals('$.Payload.status', 'FAILED'), timeoutFail);
+
+    const map = new sfn.Map(this, 'Parallelized timers', {
+      inputPath: sfn.JsonPath.entirePayload,
+      itemsPath: sfn.JsonPath.stringAt('$.timers'),
+      maxConcurrency: 50,
+      parameters: {
+        'timeoutData.$': '$$.Map.Item.Value',
+        'orderId.$': '$.orderId'
+      }
     });
 
+    // Add the steps to the parallelized state machine (in order)
+    map
+      .iterator(
+        convertTimeoutToTimestampTask
+          .next(waitState)
+          .next(checktimeoutTask)
+          .next(choiceState)
+      );
+
+    new sfn.StateMachine(this, 'OrderSlaAlerts', {
+      definition: map,
+    });
+  }
+
+  /**
+   * Creates a task state that will take the step function input and convert
+   * the timeout in seconds SLA to a timestamp. We'll use this timeout to tell
+   * the wait state how long to wait.
+   */
+  getConvertTimeoutTask = (): LambdaInvoke => {
+    // Converts timeout in seconds to a timestamp that cna be used by wait state
+    const convertTimeoutToTimestampLambda = new lambda.Function(
+      this,
+      'ConvertTimeoutToTimestamp',
+      {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: 'convert-timeout-to-timestamp.handler',
+        code: lambda.Code.fromAsset(join(__dirname, 'lambdas')),
+      }
+    );
+
+    return new sfnTasks.LambdaInvoke(this, 'CreateTimestamps', {
+      lambdaFunction: convertTimeoutToTimestampLambda,
+      payload: sfn.TaskInput.fromObject({
+        orderId: sfn.JsonPath.stringAt('$.orderId'),
+        timeoutInSeconds: sfn.JsonPath.numberAt('$.timeoutData.timeoutInSeconds'),
+      }),
+      resultSelector: {
+        'isoString.$': '$.Payload'
+      },
+
+      // Add waitTimestamp as input to next state
+      resultPath: sfn.JsonPath.stringAt('$.waitTimestamp')
+    });
+  };
+
+  /**
+   * Creates the Lambda function that will check to see if the
+   * order has reached the expected state after waiting for SLA
+   * and then returns the state machine activity.
+   */
+  getCheckTimeoutTask = (): LambdaInvoke => {
+    // Lambda function that checks to see if some work has been completed after timeout
     const checkTimeout = new lambda.Function(this, 'CheckOrderStatus', {
       runtime: lambda.Runtime.NODEJS_14_X,
       handler: 'check-status.handler',
       code: lambda.Code.fromAsset(join(__dirname, 'lambdas')),
     });
 
-    const successState = new sfn.Succeed(this, 'Timers Checked');
-
-    const calculateTimerTask = new sfnTasks.LambdaInvoke(this, 'CreateTimers', {
-      lambdaFunction: createTimers,
-      payload: sfn.TaskInput.fromObject({
-        orderId: sfn.JsonPath.stringAt('$.orderId'),
-        createdTimestamp: sfn.JsonPath.numberAt('$.createdTimestamp'),
-      }),
-      resultPath: sfn.JsonPath.stringAt('$.timers')
-    });
-
-    const checktimeoutTask = new sfnTasks.LambdaInvoke(this, 'CheckTimeout', {
+    return new sfnTasks.LambdaInvoke(this, 'CheckTimeout', {
       lambdaFunction: checkTimeout,
+
+      // Map state machine input to task state input
       payload: sfn.TaskInput.fromObject({
         orderId: sfn.JsonPath.stringAt('$.orderId'),
-        expectedState: sfn.JsonPath.stringAt('$.expectedState'),
-        alarmName: sfn.JsonPath.stringAt('$.alarmName'),
+        expectedState: sfn.JsonPath.stringAt('$.timeoutData.expectedState'),
+        alarmName: sfn.JsonPath.stringAt('$.timeoutData.alarmName'),
       }),
-      resultPath: "$"
+
+      // Overwrite previous state machine input with output from this task
+      resultPath: '$'
     });
-
-    const waitState = new sfn.Wait(this, 'Wait for Timer', {
-      time: sfn.WaitTime.timestampPath('$.waitTimestamp'),
-    });
-
-    const map = new sfn.Map(this, 'Parallelized timers', {
-      inputPath: sfn.JsonPath.entirePayload,
-      itemsPath: sfn.JsonPath.stringAt('$.timers.Payload'),
-      maxConcurrency: 50,
-    });
-
-    map
-      .iterator(waitState)
-      .iterator(checktimeoutTask)
-
-    const definition = calculateTimerTask.next(map).next(successState);
-
-    new sfn.StateMachine(this, 'OmsTimeoutAlerts', {
-      definition,
-    });
-  }
+  };
 }
+
